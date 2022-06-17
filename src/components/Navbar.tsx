6fbff3c615bef26fceeb68116023f0650ee06735
shamar-morrison <shamar.webdev@gmail.com>
2022-06-17 17:18:50 -0500

    Added Routes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>Navabr</p>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/ExerciseDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExerciseDetail = () => {
+  return (
+    <div>
+      <p>ExerciseDetail</p>
+    </div>
+  );
+};
+
+export default ExerciseDetail;
